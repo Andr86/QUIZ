d89f30e0f6435cfcd0b2d15ac3b35cd7c7538536
Andrey Sergeev <homemail45@gmail.com>
2022-07-11 15:34:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Student:
+    def __init__(self, name, surname, gender ):
+        self.name = ''
+        self.surname = ''
+        self.gender = ''
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.finished_courses += ['Git']
+best_student.courses_in_progress += ['Python']
+best_student.grades['Git'] = [10, 10, 10, 10, 10]
+best_student.grades['Python'] = [10, 10]
+
+print(best_student.finished_courses)
+print(best_student.courses_in_progress)
+print(best_student.grades)
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+print(cool_mentor.courses_attached)
+
